f41fc49bc6316db8c66b0ed0b3be4683b06478f4
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-02-06 08:52:19 +0100

    Update for latest pkg-config

--- portaudio-sys/build.rs
+++ portaudio-sys/build.rs
@@ -1,8 +1,7 @@
 extern crate "pkg-config" as pkg_config;
 
 fn main() {
-    let opts = pkg_config::default_options("portaudio-2.0");
-    match pkg_config::find_library_opts("portaudio-2.0", &opts)
+    match pkg_config::find_library("portaudio-2.0")
     {
         Ok(..) => {},
         Err(e) => panic!("{}", e),
