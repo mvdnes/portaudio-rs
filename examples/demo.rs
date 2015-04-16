950968515a308dca3b75c5db3699354c185a4ad1
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-04-16 09:37:36 +0200

    Use a barrier instead of thread::scoped

--- examples/demo.rs
+++ examples/demo.rs
@@ -40,8 +40,11 @@ fn demo() -> portaudio::PaResult
         if phase > 1.0 { phase -= 2.0; }
     }
 
-    let waiter = std::thread::scoped(|| {
+    let barrier = std::sync::Arc::new(std::sync::Barrier::new(2));
+    let sleep = barrier.clone();
+    std::thread::spawn(move|| {
         std::thread::sleep_ms((SECONDS * 1000) as u32);
+        barrier.wait();
     });
 
     match stream.write(&*buffer)
@@ -56,7 +59,7 @@ fn demo() -> portaudio::PaResult
         Ok(()) => {},
     }
 
-    let _ = waiter.join();
+    sleep.wait();
 
     Ok(())
 }
