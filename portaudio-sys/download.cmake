880848f134a452ce781c36fd92625514acd2176d
vanderlokken <vanderlokken@gmail.com>
2017-05-21 14:08:35 +0300

    Added checksum verification

--- portaudio-sys/download.cmake
+++ portaudio-sys/download.cmake
@@ -1,6 +1,7 @@
 set(url http://portaudio.com/archives/pa_stable_v190600_20161030.tgz)
 set(archive "$ENV{OUT_DIR}/portaudio.tgz")
 
-file(DOWNLOAD "${url}" "${archive}")
+file(DOWNLOAD "${url}" "${archive}"
+    EXPECTED_HASH MD5=4df8224e047529ca9ad42f0521bf81a8)
 execute_process(COMMAND "${CMAKE_COMMAND}" -E tar xvf "${archive}"
     WORKING_DIRECTORY "$ENV{OUT_DIR}")
